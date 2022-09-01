ef245fc49fc2559e2e52240c367a43e56f1409b2
Daniel Cachapa <cachapa@gmail.com>
2022-09-01 13:41:32 +0200

    Implement Linux, macOS and Windows

--- lib/save_media.dart
+++ lib/save_media.dart
@@ -1,3 +1,8 @@
+import 'dart:io';
+
+import 'package:path_provider/path_provider.dart';
+import 'package:path/path.dart' as p;
+
 import 'media_asset.dart';
 import 'save_media_platform_interface.dart';
 
@@ -5,7 +10,16 @@ export 'media_asset.dart';
 
 class SaveMedia {
 
-  Future<bool> saveMediaAssets(List<MediaAsset> mediaAssets) {
-    return SaveMediaPlatform.instance.saveMediaAssets(mediaAssets);
+  Future<bool> saveMediaAssets(List<MediaAsset> mediaAssets) async {
+    if (Platform.isAndroid || Platform.isIOS) {
+      return SaveMediaPlatform.instance.saveMediaAssets(mediaAssets);
+    }
+
+    final downloadsPath = (await getDownloadsDirectory())!.path;
+    for (final asset in mediaAssets) {
+      final newPath = p.join(downloadsPath, p.basename(asset.file.path));
+      await asset.file.copy(newPath);
+    }
+    return true;
   }
 }
